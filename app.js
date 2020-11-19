7173fff95ffdbcb3c8c5b127b4d7715cae3249bc
Rupal Jain <rupaljain9727@gmail.com>
2020-11-19 16:13:18 +0530

    Updated changes for server

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict'
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -12,6 +10,8 @@ server.listen(port, () => {
 });
 
 app.use(express.static("."));
+process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
 app.get("/", function(req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -43,29 +43,41 @@ async function runSample(sessionPath, sessionClient, question) {
     // Send request and log result
     const responses = await sessionClient.detectIntent(request);
     console.log('Detected intent');
+    // console.log(responses);
     const result = responses[0].queryResult;
-    console.log(`Query: $ { result.queryText }`);
-    console.log(`Response: $ { result.fulfillmentText }`);
+    console.log(`  Query: ${result.queryText}`);
+
+    console.log(`  Response: ${result.fulfillmentText}`);
     if (result.intent) {
-        console.log(`Intent: $ { result.intent.displayName }`);
+        console.log(`  Intent: ${result.intent.displayName}`);
     } else {
-        console.log(`No intent matched.`);
+        console.log(`  No intent matched.`);
     }
-    return result;
+
+
+    if (result.fulfillmentMessages.length == 1) {
+        var ans = result.fulfillmentMessages[0].text.text;
+    } else {
+        var ans = "";
+        for (var i = 0; i < result.fulfillmentMessages.length; i++) {
+            ans += ` ${i+1}.${result.fulfillmentMessages[i].text.text} \n `;
+        }
+    }
+    // console.log(ans);
+    return ans;
 }
 
 // To recieve events on server side
 io.on('connection', function(socket) {
     console.log('Socket connected...');
 
-    console.log(process.env);
+    // console.log(process.env);
 
     // A unique identifier for the given session
     const sessionId = uuid.v4();
 
     // Create a new session
     const sessionClient = new dialogflow.SessionsClient();
-    // const sessionPath = sessionClient.projectAgentSessionPath('student-support-system-294109', sessionId);
     const sessionPath = sessionClient.sessionPath('student-support-system-294109', sessionId);
 
     socket.on('chat', async function(data) {
@@ -73,7 +85,8 @@ io.on('connection', function(socket) {
         console.log("Response: " + JSON.stringify(reply));
 
         var response = {
-            message: reply.fulfillmentText
+            // message: reply.fulfillmentText
+            message: JSON.stringify(reply)
         }
         socket.emit('chat', response);
 
